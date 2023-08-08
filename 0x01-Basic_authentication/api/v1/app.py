1e38b4943ef935d89c1c559e97af7a49b2e29bf8
leulkashay <leulkashay2012@gmail.com>
2023-08-08 18:18:49 +0000

    New

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -41,7 +41,7 @@ def before_request():
     if auth.current_user(request) is None:
         abort(403)
 
-        
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    raise a 401 error by using abort
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    raise a 403 (forbidden) error by using abort
+    """
+    abort(403)
